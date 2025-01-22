2227a5200e8020652db1a059fec82d71b11dda42
LangArthur <arthur.lang.pro@gmail.com>
2025-01-22 10:29:09 +0100

    refacto: remove useless stuff

--- src/main/java/fr/obeeron/lockrp/keyring/KeyringInventoryListener.java
+++ src/main/java/fr/obeeron/lockrp/keyring/KeyringInventoryListener.java
@@ -14,10 +14,9 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Objects;
-
 public class KeyringInventoryListener implements Listener {
 
+    @SuppressWarnings("unused")
     private final LockRP plugin;
 
     public KeyringInventoryListener(LockRP plugin) {
@@ -26,22 +25,17 @@ public class KeyringInventoryListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
-        if (e.getClickedInventory() == null || !( e.getInventory().getHolder() instanceof KeyringInventory))
+        if (e.getClickedInventory() == null || !(e.getInventory().getHolder() instanceof KeyringInventory))
             return;
 
-        KeyringInventory keyringInventory = (KeyringInventory) e.getInventory().getHolder();
-        ItemStack keyRingItem = keyringInventory.getKeyringItem();
-
         ItemStack currentItem = e.getCurrentItem();
-        ItemStack cursorItem = e.getCursor();
 
         // Player can only interact with keys
         if ((!LRPCore.isKey(currentItem)) && currentItem != null && currentItem.getType() != Material.AIR) {
             e.setCancelled(true);
         }
         // If players tries to shift click only allow it for keys
-        else if (e.getClick() == ClickType.SHIFT_LEFT || e.getClick() == ClickType.SHIFT_RIGHT)
-        {
+        else if (e.getClick() == ClickType.SHIFT_LEFT || e.getClick() == ClickType.SHIFT_RIGHT) {
             if (currentItem != null && !LRPCore.isKey(currentItem)) {
                 e.setCancelled(true);
             }
@@ -58,11 +52,11 @@ public class KeyringInventoryListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e){
+    public void onInventoryClose(InventoryCloseEvent e) {
         Inventory inventory = e.getInventory();
 
         if (inventory.getHolder() instanceof KeyringInventory keyRingInventory &&
-            e.getPlayer() instanceof Player player) {
+                e.getPlayer() instanceof Player player) {
             player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_CHAIN, 1, 1);
             keyRingInventory.onClose(e.getPlayer().getInventory());
         }
